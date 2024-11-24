4f4d010a5d0bfbdbc212ee0ddfb8b56a61387be1
linkroot1 <lekha-now@mail.ru>
2024-11-24 18:16:09 +0300

    Вынес логику из контроллеров в сервисы

--- src/main/java/com/example/books/controllers/BookController.java
+++ src/main/java/com/example/books/controllers/BookController.java
@@ -1,68 +1,53 @@
 package com.example.books.controllers;
 
 
-import com.example.books.dao.BookDAO;
 import com.example.books.models.Book;
+import com.example.books.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
 public class BookController {
 
-    private final BookDAO bookDAO;
-
     @Autowired
-    public BookController(BookDAO bookDAO) {
-        this.bookDAO = bookDAO;
-    }
+    private BookService bookService;
 
     @GetMapping(path = "/books", produces = "application/json")
     public List<Book> getList() {
-        return bookDAO.getList();
+        return bookService.getList();
     }
 
     @GetMapping(path = "/sorted-books", produces = "application/json")
     public List<Book> getSortedByTitleList() {
-        List<Book> books = bookDAO.getList();
-
-        //Сортировка списка книг по названию в обратном алфавитном порядке
-        return books.stream()
-                .sorted(Comparator.comparing(Book::getTitle).reversed())
-                .collect(Collectors.toList());
+        return bookService.getSortedByTitleList();
     }
 
     @GetMapping(path = "/grouped-books", produces = "application/json")
     public Map<String, List<Book>> getGroupByAuthorList() {
-        List<Book> books = bookDAO.getList();
-
-        //Группировка книг по автору
-        return books.stream()
-                .collect(Collectors.groupingBy(Book::getAuthor));
+        return bookService.getGroupByAuthorList();
     }
 
     @PostMapping(path = "/create", produces = "application/json")
     public void create(@RequestBody Book book) {
-        bookDAO.save(book);
+        bookService.create(book);
     }
 
     @PostMapping(path = "/update/{bookId}", produces = "application/json")
-    public void create(@PathVariable int bookId, @RequestBody Book book) {
-        bookDAO.update(bookId, book);
+    public void update(@PathVariable int bookId, @RequestBody Book book) {
+        bookService.update(bookId, book);
     }
 
     @DeleteMapping(path = "/delete/{bookId}")
     public void delete(@PathVariable int bookId) {
-        bookDAO.delete(bookId);
+        bookService.delete(bookId);
     }
 
     @GetMapping(path = "/{bookId}", produces = "application/json")
     public Book get(@PathVariable int bookId) {
-        return bookDAO.get(bookId);
+        return bookService.get(bookId);
     }
 }
